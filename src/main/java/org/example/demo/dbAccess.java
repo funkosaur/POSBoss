0f5a96ea2036635d3079c4dca0eb8fbe237d1a7e
Andrei <SoboAndrei@gmail.com>
2024-03-09 22:09:15 +0000

    .

--- src/main/java/org/example/demo/dbAccess.java
+++ src/main/java/org/example/demo/dbAccess.java
@@ -5,24 +5,38 @@ public class dbAccess
 {
   public void loadDriver() throws Exception
   {
-    // Load MySQL JDBC driver
-    Class.forName("com.mysql.cj.jdbc.Driver");
-    
+    System.out.println("loading linux access driver");
+    try {
+      Class.forName("com.mysql.cj.jdbc.Driver");
+      // Class.forName("com.mysql.jdbc.Driver");
+      System.out.println("Driver loaded successfully");
+    } catch (Exception e) {
+      System.out.println("Failed to load driver");
+      System.out.println("Failed to load driver: " + e.getMessage());
+      throw e;
+    }
   }
 
   public String urlOfDatabase()
   {
-
-    return "jdbc:mysql://178.128.37.54:3306/as2491_POS";
+    System.out.println("loading linux access database url");
+    String url = "jdbc:mysql://178.128.37.54:3306/as2491_POS";
+    System.out.println("Database URL: " + url);
+    return url;
   }
 
   public String username()
   {
-    return "as2491_POS";
+    String username = "as2491_POS";
+    System.out.println("Username: " + username);
+    return username;
   }
 
   public String password()
   {
-    return "PosBOSS420";
+    String password = "PosBOSS420";
+    System.out.println("Password: " + password);
+    return password;
+    // String password = "Deoareceichtyandro123";
   }
 }
